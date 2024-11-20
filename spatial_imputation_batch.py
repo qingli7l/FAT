9b04e794ca0965693da7403250e54ec6400eaa3a
Ashen <liqingchdu@163.com>
2024-11-20 15:02:44 +0800

    Update and rename spatial_imputation_task.py to spatial_imputation_batch.py

--- spatial_imputation_batch.py
+++ spatial_imputation_batch.py
@@ -1,5 +1,5 @@
 import argparse
-import anndata as ad
+import scanpy as sc
 import numpy as np
 import pandas as pd
 import scipy.stats as st
@@ -7,7 +7,6 @@ from scipy.stats import entropy
 from sklearn.preprocessing import MinMaxScaler
 import torch
 import os
-import hdf5plugin
 from tqdm import tqdm
 from model_fat import *
 from dataset import *
@@ -39,7 +38,7 @@ def FAT(st_train, sc_train, st_test, sc_test, args):
     
     # ori data
     sc_ori_scaled = pd.DataFrame(sc_train.X.toarray(), columns=sc_train.var_names)
-    st_ori_scaled = pd.DataFrame(st_train.X.toarray(), columns=st_train.var_names) # index=axis_index,
+    st_ori_scaled = pd.DataFrame(st_train.X.toarray(), columns=st_train.var_names) 
 
     # shared ori data
     sc_intsec_scaled = sc_ori_scaled[np.intersect1d(sc_ori_scaled.columns,st_ori_scaled.columns)]
@@ -62,7 +61,6 @@ def FAT(st_train, sc_train, st_test, sc_test, args):
 
         l_proj = -F.cosine_similarity(cell2voxel, y_dic, dim=0).mean() \
             -F.cosine_similarity(torch.matmul(W, sc_input[:,:st_ori_scaled.shape[1]]), st_input[:,:st_ori_scaled.shape[1]], dim=0).mean()\
-
         
         loss =   args.proj * l_proj  + l_align 
         
@@ -82,36 +80,49 @@ def FAT(st_train, sc_train, st_test, sc_test, args):
 
          
 if __name__ == '__main__':
-    DATASET = 'Liver'
-    # DATASET = 'Lung'
+    DATASET = 'zhuang'
 
-    # data prepare
-    direction='src/human_data/'
-    if DATASET == 'Lung':
-        query_dataset = os.path.join(direction,'HumanLungCancerPatient2_filtered_ensg.h5ad')
-        ref_dataset = os.path.join(direction,'GSE131907_Lung_ensg.h5ad')
-        st_ori = ad.read_h5ad(query_dataset)
-        sc_ori = ad.read_h5ad(ref_dataset)
-    elif DATASET == 'Liver':
-        query_dataset = os.path.join(direction,'HumanLiverCancerPatient2_filtered_ensg.h5ad')
-        ref_dataset = os.path.join(direction,'GSE151530_Liver_ensg.h5ad')
-        st_ori = ad.read_h5ad(query_dataset)
-        sc_ori = ad.read_h5ad(ref_dataset)
-    print('original sc shape: ',sc_ori.X.shape, 'st shape: ',st_ori.X.shape)
+    batch_num = 1 
+    # batch_num = 2
+    # batch_num = 3 
+    # batch_num = 4
+
+    sc_ori = sc.read_h5ad("src/human_data/dataset_zhuang/sc.h5ad")
+    if batch_num == 1:
+        st_ori = sc.read_h5ad("src/human_data/dataset_zhuang/st1.h5ad") 
+    if batch_num == 2:
+        st_ori = sc.read_h5ad("src/human_data/dataset_zhuang/st2.h5ad")
+    if batch_num == 3:
+        st_ori = sc.read_h5ad("src/human_data/dataset_zhuang/st3.h5ad")
+    if batch_num == 4:
+        st_ori = sc.read_h5ad("src/human_data/dataset_zhuang/st4.h5ad")
+    sc_ori.obs['batch']= sc_ori.obs['cluster'] 
+    st_ori.obs['batch']= st_ori.obs['brain_section_label_y']
+    print('ori sc shape: ',sc_ori.X.shape, 'st shape: ',st_ori.X.shape)
     
-    # data filter
+    # filter lowely expressed genes
     sc_ori.obs['Genes_count'] = np.sum(sc_ori.X > 0, axis=1) 
     sc_ori = sc_ori[sc_ori.obs['Genes_count'] >=10] 
     st_ori.obs['Genes_count'] = np.sum(st_ori.X > 0, axis=1) 
-    st_ori = st_ori[st_ori.obs['Genes_count'] >=10] 
+    st_ori = st_ori[st_ori.obs['Genes_count'] >=10]
+    
+    shared_gene = pd.read_csv('src/batch_shared_genes.txt', sep='\t', header=None)[1].tolist()
+    shared_gene = [gene.strip() for gene in shared_gene]
+    shared_gene = [gene for gene in shared_gene if gene in sc_ori.var_names]
+
+    sc_ori = sc_ori[:,shared_gene]
+    st_ori = st_ori[:,shared_gene]
+    
+    sc_ori = sc_ori[:1000,:]
+    st_ori = st_ori[:500,:]
+    
     print('filtered sc shape: ',sc_ori.X.shape, 'st shape: ',st_ori.X.shape)
     
-    shared_gene = np.intersect1d(sc_ori.var_names,st_ori.var_names)
-    sc_ori = sc_ori[:105,shared_gene]
-    st_ori = st_ori[:105,shared_gene]
-    
+    # sample 25 genes of each group in sparcity boundaries = [0, 0.75, 0.9, 0.95, 1]
     target_genes = stratified_sample_genes_by_sparsity(st_ori, seed=11)
     
+    imput_gene = shared_gene[2]
+    target_genes.append(imput_gene)
     num = len(target_genes)
     parser = argparse.ArgumentParser(description='Gene Alignment Projection Model')
     parser.add_argument('--dataset_num', default=DATASET, type=str, help='the number of target genes')
@@ -126,9 +137,12 @@ if __name__ == '__main__':
     parser.add_argument('--wd', default=1e-8, type=int, help='weight decay')
     parser.add_argument('--epoch', default=20, type=int, help='training epoch')
     parser.add_argument("--n_splits", type=int, default=5, help='cross-validation sets number')
+    
+    parser.add_argument('--batch_num', default=batch_num, type=int, help='batch num')
+    
     args = parser.parse_args()
     print(args)
-    
+
     # Prediction results
     Correlations = pd.Series(index = target_genes) 
     PCC = pd.Series(index = target_genes) 
@@ -142,7 +156,6 @@ if __name__ == '__main__':
     Dic_data =  pd.DataFrame({'column': [np.zeros((sc_ori.shape[0], st_ori.shape[0])) for _ in target_genes]}, index=target_genes)
     scaler = MinMaxScaler(feature_range=(0, 1))
     
-    # training process
     for i in tqdm(target_genes):
         Imp_Genes, Dic  = FAT(st_ori[:, ~st_ori.var_names.isin([i])], sc_ori[:, ~st_ori.var_names.isin([i])],
                         st_ori[:, st_ori.var_names.isin([i])], sc_ori[:, st_ori.var_names.isin([i])], args)
@@ -177,7 +190,7 @@ if __name__ == '__main__':
     print('MAE: ', MAE.values)
     print('MSE: ', MSE.values)
     print('COS: ', COS.values)
-    path = os.path.join(args.dir, 'fat_data_{}_gene_{}_ldim_{}.txt'.format(DATASET, args.test_gene_num, args.latent_dim))
+    path = os.path.join(args.dir, 'fat_data_{}_gene_{}_ldim_{}_batch_{}.txt'.format(DATASET, args.test_gene_num, args.latent_dim, batch_num))
     with open(path, 'w') as f:
         for item in Correlations.index:
             f.write(str(item) + '\n')
@@ -205,11 +218,8 @@ if __name__ == '__main__':
         f.write('Average_COS_data_'+ str(DATASET)  + '\n'+ str(np.mean(COS.values)) + '\n\n')
     
     # draw results
-    gene_num = [0]
-    for i in gene_num:
-        draw_data_batch(target_genes[i], sc_ori, st_ori, All_Imp_Genes, DATASET, args)
+    draw_data_batch(imput_gene, sc_ori, st_ori, All_Imp_Genes, DATASET, args)
 
 
 # conda activate fat      
-# CUDA_VISIBLE_DEVICES='0' python spatial_imputation_task.py
-
+# CUDA_VISIBLE_DEVICES='0' python spatial_imputation_batch.py
